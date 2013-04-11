4ce941a4c76c6f7f05c2a936bf49502a04abd2f7
Carl <cdsimpkins@gmail.com>
2013-04-11 19:12:30 -0400

    first commit

---
+++ SchoolCommand/PeopleManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCommand
+{
+    class PeopleManager
+    {
+        public string type
+        {
+            get
+            {
+                return this._type;
+            }
+        }
+
+        public int mode
+        {
+            get
+            {
+                return this._mode;
+            }
+        }
+
+        private string _type;
+        private int _mode;
+
+        public PeopleManager(int mode)
+        {
+            switch (mode)
+            {
+                case 1:
+                    _type = "Person";
+                    this._mode = mode;
+                    break;
+                case 2:
+                    _type = "Student";
+                    this._mode = mode;
+                    break;
+                case 3:
+                    _type = "Teacher";
+                    this._mode = mode;
+                    break;
+            }
+        }
+
+        public void AddPerson(Person p)
+        {
+
+            using (var db = new Entities())
+            {
+                if (p is Students)
+                {
+                    Person output = db.People.Add(p);
+                    
+                }
+
+                else if (p is Teachers)
+                { }
+                else if (p is CaseManagers)
+                { }
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ SchoolCommand/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1) Manage People\n2) Manage Rooms\n3) Create Schedules\n4) Print Schedules");
+            int choice=0;
+            while (choice == 0)
+            {
+                try
+                {
+                    
+                    choice = Convert.ToInt16(Console.ReadKey().KeyChar);
+
+                    if (choice == 1)
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("1) Manage Students\n2) Manage Teachers\n3) Manage Case Managers");
+                            
+                            choice = Convert.ToInt16(Console.ReadKey().KeyChar);
+
+                            PeopleManager m = new PeopleManager(choice);
+
+                            Console.WriteLine("1: Add " + m.type + "\n2: Edit " + m.type + "\n3: Delete " + m.type
+                                + "4: Exit");
+                            
+                            choice = Convert.ToInt16(Console.ReadKey().KeyChar);
+
+                            if (choice == 1)
+                            {
+                                Console.Write("Name: ");
+                                String name = Console.ReadLine();
+                                Console.Write("Address: ");
+                                String address = Console.ReadLine();
+                                Console.Write("Age: ");
+                                String age = Console.ReadLine();
+                                Console.Write("Phone: ");
+                                String phone = Console.ReadLine();
+                                var person = new Person();
+                                if (m.type.Equals("Case Manager"))
+                                {
+                                    person = new CaseManagers
+                                    {
+                                        Name = name,
+                                        Phone = phone,
+                                        Age = age,
+                                        Address = address
+                                    };
+                                }
+                                else
+                                {
+                                    //prompt for specialty, obtain specialty
+                                    
+                                    if (m.type.Equals("Student"))
+                                    {
+
+                                        person = new Students
+                                        {
+                                            Name = name,
+                                            Phone = phone,
+                                            Age = age,
+                                            Address = address
+                                        };
+                                    }
+                                    else if (m.type.Equals("Teacher"))
+                                    {
+                                        person = new Teachers
+                                        {
+                                            Name = name,
+                                            Phone = phone,
+                                            Age = age,
+                                            Address = address
+                                        };
+                                    }
+                                }
+                                m.AddPerson(person);
+
+                            }
+                        }
+                    }
+                    else if (choice == 2)
+                    { }
+                    else if (choice == 3)
+                    { }
+                    else if (choice == 4)
+                    { }
+                }
+                catch (InvalidCastException e)
+                {
+                    Console.WriteLine("Please choose a number");
+                }
+            }
+        }
+    }
+}
